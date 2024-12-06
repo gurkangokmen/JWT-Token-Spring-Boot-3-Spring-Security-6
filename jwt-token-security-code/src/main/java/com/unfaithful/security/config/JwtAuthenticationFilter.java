06dfd2e54f7a7aaa7f8fe72ff23274bc8cf60c9b
Gürkan Gökmen <122023578+gurkangokmen@users.noreply.github.com>
2024-12-06 19:37:43 +0300

    JwtAuthFilter

---
+++ jwt-token-security-code/src/main/java/com/unfaithful/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,34 @@
+package com.unfaithful.security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+// Run at every request
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return; //We don't want to continue if there is no token
+        } else {
+            jwt = authHeader.substring(7);
+        }
+    }
+}
---
+++ jwt-token-security-code/src/main/java/com/unfaithful/security/user/Role.java
@@ -0,0 +1,5 @@
+package com.unfaithful.security.user;
+
+public enum Role {
+    USER, ADMIN
+}
